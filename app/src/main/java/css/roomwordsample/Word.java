dbdb8f3047e60562a223ef750a333e2f013bb5da
patriotfever4@gmail.com <Ek@Z0463>
2019-04-02 23:11:27 -0500

    Completed Unit 10 Assignment

--- app/src/main/java/css/roomwordsample/Word.java
+++ app/src/main/java/css/roomwordsample/Word.java
@@ -5,20 +5,17 @@ import android.arch.persistence.room.Entity;
 import android.arch.persistence.room.PrimaryKey;
 import android.support.annotation.NonNull;
 
+//Creates Entity for the word table
 @Entity(tableName = "word_table") //defines the enitity of the table
 public class Word {
 
-    //helps autogenerate unique keys for each primary key
-    @PrimaryKey(autoGenerate = true)
-    private int id;
-
-    @NonNull //states that no parameters, fields or values can be numm
-    private String word;
-
-    @ColumnInfo(name = "word") //specifies the name of the column of the table
+    @PrimaryKey
+    @NonNull
+    @ColumnInfo(name = "word")
     private String mWord;
 
     public Word(String word) {this.mWord = word;}
 
     public String getWord(){return this.mWord;}
+
 }
--- app/src/main/java/css/roomwordsample/WordRoomDatabase.java
+++ app/src/main/java/css/roomwordsample/WordRoomDatabase.java
@@ -1,9 +1,12 @@
 package css.roomwordsample;
 
+import android.arch.persistence.db.SupportSQLiteDatabase;
 import android.arch.persistence.room.Database;
 import android.arch.persistence.room.Room;
 import android.arch.persistence.room.RoomDatabase;
 import android.content.Context;
+import android.os.AsyncTask;
+import android.support.annotation.NonNull;
 
 //Generates the Room layer of aplication, handles SQLiteOpenHelper
 @Database(entities = {Word.class}, version = 1)
@@ -19,10 +22,52 @@ public abstract class WordRoomDatabase extends RoomDatabase {
                 if (INSTANCE == null) {
                     INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
                             WordRoomDatabase.class, "word_database")
+                            .addCallback(sRoomDatabaseCallback)
                             .build();
                 }
             }
         }
         return INSTANCE;
     }
+
+    //Deletes all content from application and repoulates data when app is started
+    private static RoomDatabase.Callback sRoomDatabaseCallback =
+            new RoomDatabase.Callback() {
+
+                @Override
+                public void onOpen(@NonNull SupportSQLiteDatabase db) {
+                    super.onOpen(db);
+                    new PopulateDbAsync(INSTANCE).execute();
+                }
+            };
+
+    //Deletes content of the database and populates application with given words
+    private static class PopulateDbAsync extends AsyncTask<Void, Void, Void> {
+
+        private final WordDao mDao;
+
+        PopulateDbAsync(WordRoomDatabase db) {
+            mDao = db.wordDao();
+        }
+
+        @Override
+        protected Void doInBackground(final Void... params) {
+            mDao.deleteAll();
+
+            Word word = new Word("Hello");
+            mDao.insert(word);
+            word = new Word("World");
+            mDao.insert(word);
+            word = new Word("Programming");
+            mDao.insert(word);
+            word = new Word("Awesome");
+            mDao.insert(word);
+            word = new Word("College");
+            mDao.insert(word);
+            word = new Word("Scholastica");
+            mDao.insert(word);
+            return null;
+        }
+    }
+
 }
